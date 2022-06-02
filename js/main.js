cfe7faa8aacbfa10b28eaaed9d1e868ea7e28756
Melldy Monteverde <monteverdelex@gmail.com>
2022-06-02 20:46:51 -0300

    agrega prompt para pedir datos y funcion para saludar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 alert("Hola usuario, esta pagina sigue en construccion!")
 alert("Solo podras navegar y ver nuestros productos pero no podras realizar compras desde este sitio, podras dirigrte a nuestras sucursales o hacer pedido por tlf! Gracias por ser paciente!")
 
-let preguntar = prompt("Mientras, te gustaria dejarnos tus datos de contacto?")
+let username
+let phone
+let mail
+
+debugger
+function saludar() {
+    username = prompt("Ingresa tu nombre:")
+    
+    if (username == null) {
+        alert("no ingresaste tu nombre")
+    } else {
+        mail = prompt("ingresa tu mail:")
+        phone = parseInt(prompt("ingresa tu cel:"))
+        alert("hola " + username + " gracias por dejar tu info de contacto")
+    }
+}
+
+saludar()
+
