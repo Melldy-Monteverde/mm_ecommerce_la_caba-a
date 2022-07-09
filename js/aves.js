64d6c97bded9ad8d9191d4d8f96ae89c4b128500
Melldy Monteverde <monteverdelex@gmail.com>
2022-07-09 18:47:06 -0300

    agrega api local en formato json y se consume en aves.js como ejemplo

--- js/aves.js
+++ js/aves.js
@@ -1,28 +1,44 @@
 // creo una constante para el contenedor de los productos
 let carritoDeCompras = [];
 
-
 const contenedorItems = document.getElementById('section__items');
 const contenedorCarrito = document.getElementById('carrito-contenedor');
 
 const contadorCarrito = document.getElementById('contadorCarrito');
 const btnVaciar = document.getElementById('btnVaciarCarrito');
+const btnCarrito = document.getElementById('btnCarrito');
 
 const btnTerminar = document.getElementById('terminar');
 const precioTotal = document.getElementById('precioTotal');
 
-mostrarArticulosAves();
+// aqui consumimos la api local en formato json
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+});
 
-function mostrarArticulosAves() {
-    stockProductosAves.forEach(el => {
+const fetchData = async () => {
+    try {
+        const res = await fetch('../api.json');
+        const data = await res.json();
+
+        mostrarProductos(data)
+    } catch(error) {
+        console.log(error)
+    }
+}
+// esta funcion reemplaza a mostrarArticulosAves(), consume la api y muestra la plantilla
+const mostrarProductos = data => {
+    data.forEach(el => {
         let article = document.createElement('article');
         article.classList.add('items__gallery');
-        article.innerHTML = `<img src="${el.img}" alt="${el.alt}">
+        article.innerHTML = `
+                            <img src="${el.img}" alt="${el.alt}">
                             <h5>${el.nombre}</h5>
                             <p>$${el.precio}</p>
                             <span>${el.aviso}</span>
                             <button id="btnA${el.id}" class="btn-agregar">Agregar al carrito</button>
-                            <button id="btnB${el.id}" class="btn-borrar">Borrar del carrito</button>`;
+                            <button id="btnB${el.id}" class="btn-borrar">Borrar del carrito</button>
+                            `;
         contenedorItems.appendChild(article);
 
         let btnAgregar = document.getElementById(`btnA${el.id}`);
@@ -37,7 +53,33 @@ function mostrarArticulosAves() {
     })
 }
 
-// esta funcion debe ser para el app.js 
+// esta era la funcion original que consumia el stock.js
+
+// function mostrarArticulosAves() {
+//     stockProductosAves.forEach(el => {
+//         let article = document.createElement('article');
+//         article.classList.add('items__gallery');
+//         article.innerHTML = `
+//                             <img src="${el.img}" alt="${el.alt}">
+//                             <h5>${el.nombre}</h5>
+//                             <p>$${el.precio}</p>
+//                             <span>${el.aviso}</span>
+//                             <button id="btnA${el.id}" class="btn-agregar">Agregar al carrito</button>
+//                             <button id="btnB${el.id}" class="btn-borrar">Borrar del carrito</button>
+//                             `;
+//         contenedorItems.appendChild(article);
+
+//         let btnAgregar = document.getElementById(`btnA${el.id}`);
+//         btnAgregar.addEventListener('click', () => {
+//             agregarAlCarrito(el.id);
+//         })
+        
+//         let btnBorrar = document.getElementById(`btnB${el.id}`);
+//         btnBorrar.addEventListener('click', () => {
+//             borrarDelCarrito(el.id);
+//         })
+//     })
+// }
 
 // agregar al carrito
 function agregarAlCarrito(id) {
@@ -46,7 +88,7 @@ function agregarAlCarrito(id) {
     mostrarCarrito(agregarProducto);
     actualizarCarrito();
 
-    almacenarCarrito()
+    almacenarCarrito();
     
     console.log(carritoDeCompras)
 }
@@ -55,6 +97,10 @@ function borrarDelCarrito(id) {
     let borrarProducto = stockProductosAves.find(item => item.id === id);
     let index = carritoDeCompras.indexOf(borrarProducto);
     carritoDeCompras.splice(index, 1);
+    
+    const carritoMenosUno = (carritoDeCompras.length === 0) ? sweetAlertVaciar() : carritoDeCompras.length - 1;
+    contadorCarrito.length - 1;
+    
     mostrarCarrito(borrarProducto);
     actualizarCarrito();
 
@@ -82,36 +128,46 @@ function mostrarCarrito(agregarProducto) {
 
 }
 
+// prevenir refresh del boton carrito
+btnCarrito.addEventListener ('click', (e) => {
+    e.preventDefault();
+})
+
 function actualizarCarrito() {
     contadorCarrito.innerText = carritoDeCompras.length;
     let total = carritoDeCompras.reduce((acc, item) => acc + item.precio, 0);
     console.log(`El valor del carrito es: $${total}`)
 }
 
-// almaceno en localStorage
-const almacenarCarrito = () => {
-    localStorage.setItem('item', JSON.stringify(carritoDeCompras))
-}
-
-// recupero el localStorage para el carrito
-const recuperarCarrito = () => {
-    const productos = JSON.parse(localStorage.getItem('item'));
-    // let item = '';
-    for(prod of productos) {
-        // aqui podria ir una lista del carrito por ahora veremos una tabla en consola
-        console.table(prod)
-        /*item += `<li>${producto}` </li>
-        ul.innerHTML = item*/
-    }
-}
-
-recuperarCarrito()
-
-// puede mejorar ?
+// aqui use el operador ternario
 btnVaciar.addEventListener('click', (e) => {
     e.preventDefault();
-    carritoDeCompras.length = 0;
+    const vaciarCarro = (carritoDeCompras.length === 0) ? sweetAlertVaciar() : carritoDeCompras.length = 0;
     contadorCarrito.length = 0;
     actualizarCarrito()
 })
+
+// alerta de carrito cuando esta vacio y se prsiona el boton vaciar
+const sweetAlertVaciar = () => {
+    Swal.fire({
+        title: 'Aviso!',
+        text: 'El carrito esta vacio!',
+        icon: 'info',
+        confirmButtonText: 'continuar'
+      })
+}
+
+// almaceno en localStorage
+const almacenarCarrito = () => {
+    localStorage.setItem('productoEnCarrito', JSON.stringify(carritoDeCompras));
+}
+
+const recuperarCarrito = () => {
+    const carritoEnLS = JSON.parse(localStorage.getItem('productoEnCarrito'));
+
+    if (carritoEnLS) {
+        carrito = carritoEnLS;
+        console.log(carritoDeCompras);
+    }
+}
 
